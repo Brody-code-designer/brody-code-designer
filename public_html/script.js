30a9c51d6808d011275ea219033ddb5b19b53331
Brody-code-designer <cslama@cnm.edu>
2021-05-12 09:06:37 -0600

    added background gif, changed review area to cards

---
+++ public_html/script.js
@@ -0,0 +1,21 @@
+window.addEventListener('load', (event) => {
+    // activateGIF()
+    // deActivateGIF()
+})
+
+//function that activates the gif when scrolled into
+const activateGIF = function () {
+    let gif = `
+    <img src="imgs/pwp-background-placeholder_4.gif" id="gifContainer" width="2500px" class="img-fluid px-0 backgroundIMG" alt="Galaxy">
+    `
+
+    document.getElementById("gifContainer").innerHTML = gif
+}
+
+const deActivateGIF = function () {
+    let img = `
+    <img src="imgs/pwp-background-placeholder.png" id="gifContainer" width="2500px" class="img-fluid px-0 backgroundIMG" alt="Galaxy">
+    `
+
+    document.getElementById("gifContainer").innerHTML = img
+}
