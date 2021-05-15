36b4067f5e85df340678ca9d619e68547343599e
Brody-code-designer <cslama@cnm.edu>
2021-05-15 07:01:32 -0600

    set up express dependencies, started express front-end validation

---
+++ src/index.js
@@ -0,0 +1,38 @@
+// Import our libraries
+const express = require("express")
+const morgan = require("morgan")
+const bodyParser = require("body-parser")
+
+//initialize Express
+const app = express()
+
+// app.use allows for different middleware to be brought into Express
+// Morgan: a logger for express so that we have a record for debugging.
+app.use(morgan("dev"))
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+const indexRoute = express.Router()
+
+
+const handleGetRequest = (request, response, next) => {
+    return(response.json("server is live"))
+}
+const handlePostRequest = (request, response, next) => {
+
+    //TODO remove when Docker is up and running
+    response.append("Access-Control-Allow-Origin", "*")
+
+    response.append("Content-type", text/html)
+
+    response.append("Access-Control-Allow-Origin", "*")
+}
+
+indexRoute.route("/")
+    .get(handleGetRequest)
+    .post(handlePostRequest)
+
+app.use("/apis", indexRoute)
+
+app.listen(4200, ()=> {console.log("express successfully built")})
